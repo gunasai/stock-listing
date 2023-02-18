fe1252d2d65aa3a7956545ced1293144ee5ab542
gunasai <garapatigunasai@gmail.com>
2023-02-18 20:51:11 +0530

    feat: added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container } from '@chakra-ui/react';
+import { StockOverviewPage } from './pages/StockOverviewPage';
+import { StockDetailPage } from './pages/StockDetailPage';
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Container centerContent>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/detail/:symbol' element={<StockDetailPage />} />
+          <Route path='/' element={<StockOverviewPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
---
+++ src/pages/StockDetailPage.jsx
@@ -0,0 +1,7 @@
+export const StockDetailPage = () => {
+  return (
+    <div>
+      <h1>Stock Details</h1>
+    </div>
+  );
+};
